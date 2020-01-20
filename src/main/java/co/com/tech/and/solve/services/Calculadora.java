f6c9a9e55eca3c0840a61fea6139cbd8487a6079
duver.carmona <duver.carmona@techandsolve.com>
2020-01-20 08:37:17 -0500

    Test debeEjecutarOperacionMultiplicacion pasa

--- src/main/java/co/com/tech/and/solve/services/Calculadora.java
+++ src/main/java/co/com/tech/and/solve/services/Calculadora.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import co.com.tech.and.solve.domain.IOperacion;
+import co.com.tech.and.solve.domain.Multiplicacion;
 import co.com.tech.and.solve.domain.Resta;
 import co.com.tech.and.solve.domain.Suma;
 
@@ -11,6 +12,7 @@ public class Calculadora {
 
 	private Suma suma = new Suma();
 	private Resta resta = new Resta();
+	private Multiplicacion multiplicacion= new Multiplicacion();
 
 
 	public double ejecutarOperacion(double numeroUno, double numeroDos, String tipoOperacion) {
@@ -19,6 +21,7 @@ public class Calculadora {
 		operaciones.put("SUMA", suma);
 		operaciones.put("RESTA", resta);
 
+		operaciones.put("MULTIPLIACION", multiplicacion);
 		
 		return operaciones.get(tipoOperacion).ejecutar(numeroUno, numeroDos); 
 		
